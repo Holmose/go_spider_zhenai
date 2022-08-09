2ef858d79359c58d40796d60a80fc23cafee977f
holmose <holmose@qq.com>
2022-08-09 09:10:15 +0800

    首次提交

---
+++ service/customer_service.go
@@ -0,0 +1,8 @@
+package service
+
+import "fmt"
+
+// code
+func TestCustomerService() {
+	fmt.Println("test customer service...")
+}
---
+++ service/user_service.go
@@ -0,0 +1,8 @@
+package service
+
+import "fmt"
+
+// code
+func TestUserService() {
+	fmt.Println("test user service...")
+}
