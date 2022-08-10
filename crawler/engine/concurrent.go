6860c4d8d8a4b7dacbfbc9d6ee19060dc785e844
holmose <holmose@qq.com>
2022-08-10 22:02:00 +0800

    珍爱网：并发版爬虫

---
+++ crawler/engine/concurrent.go
@@ -0,0 +1,59 @@
+package engine
+
+import (
+	"log"
+)
+
+type ConcurrentEngine struct {
+	Scheduler   Scheduler
+	WorkerCount int
+}
+
+type Scheduler interface {
+	Submit(Request)
+	ConfigureMasterWorkerChan(chan Request)
+}
+
+// Run 指针类型的接收者，用于改变 Scheduler
+func (e *ConcurrentEngine) Run(seeds ...Request) {
+	// 定义输入、输出通道
+	in := make(chan Request)
+	out := make(chan ParseResult)
+	// 将通道传入Scheduler
+	e.Scheduler.ConfigureMasterWorkerChan(in)
+
+	for i := 0; i < e.WorkerCount; i++ {
+		createWorker(in, out)
+	}
+
+	for _, r := range seeds {
+		e.Scheduler.Submit(r)
+	}
+
+	for {
+		// 获取输出通道的内容
+		result := <-out
+		for _, item := range result.Items {
+			log.Printf("Got item: %v", item)
+		}
+
+		// 将request 送入Scheduler
+		for _, request := range result.Request {
+			e.Scheduler.Submit(request)
+		}
+	}
+
+}
+
+func createWorker(in chan Request, out chan ParseResult) {
+	go func() {
+		for {
+			request := <-in
+			result, err := worker(request)
+			if err != nil {
+				continue
+			}
+			out <- result
+		}
+	}()
+}
--- crawler/main.go
+++ crawler/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"PRO02/crawler/engine"
+	"PRO02/crawler/scheduler"
 	"PRO02/crawler/zhenai/parser"
 )
 
@@ -11,9 +12,18 @@ func main() {
 	//	ParserFunc: parser.ParseCityList,
 	//})
 
-	engine.SimpleEngine{}.Run(engine.Request{
-		Url:        "http://www.zhenai.com/zhenghun/anhui",
-		ParserFunc: parser.ParseCity,
+	//engine.SimpleEngine{}.Run(engine.Request{
+	//	Url:        "http://www.zhenai.com/zhenghun/anhui",
+	//	ParserFunc: parser.ParseCity,
+	//})
+
+	e := engine.ConcurrentEngine{
+		Scheduler:   &scheduler.SimpleScheduler{},
+		WorkerCount: 5,
+	}
+	e.Run(engine.Request{
+		Url:        "http://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
 	})
 
 }
