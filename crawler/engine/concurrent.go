52b0bf6ad9795a09c2e9ae0ba674b7c9c3cdd029
holmose <holmose@qq.com>
2022-08-11 16:13:03 +0800

    珍爱网：并发版爬虫-增加延时访问

--- crawler/engine/concurrent.go
+++ crawler/engine/concurrent.go
@@ -29,12 +29,13 @@ func (e *ConcurrentEngine) Run(seeds ...Request) {
 	for _, r := range seeds {
 		e.Scheduler.Submit(r)
 	}
-
+	itemCount := 0
 	for {
 		// 获取输出通道的内容
 		result := <-out
 		for _, item := range result.Items {
-			log.Printf("Got item: %v", item)
+			log.Printf("Got item #%d: %v", itemCount, item)
+			itemCount++
 		}
 
 		// 将request 送入Scheduler
--- crawler/fetcher/fetcher.go
+++ crawler/fetcher/fetcher.go
@@ -38,7 +38,11 @@ const (
 	userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.47"
 )
 
+var rateLimiter = time.Tick(10 * time.Millisecond)
+
 func Fetch(url string) ([]byte, error) {
+	<-rateLimiter
+
 	client := &http.Client{
 		Timeout: 2 * time.Second,
 	}
