b328d2b769cf3b4c0e9ca9024a489b62890e3879
holmose <holmose@qq.com>
2022-08-15 20:31:03 +0800

    前端服务提示优化

--- crawler/frontend/starter.go
+++ crawler/frontend/starter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"PRO02/crawler/frontend/controller"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -12,7 +13,9 @@ func main() {
 	http.Handle("/search",
 		controller.CreateSearchResultHandler(
 			"crawler/frontend/view/template.html"))
+	fmt.Println("Frontend Server Listening 8888 ...")
 	err := http.ListenAndServe(":8888", nil)
+
 	if err != nil {
 		log.Panic(err)
 	}
